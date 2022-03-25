9ddff3953b6423d67eaa2a9180505a7c97cda3dc
Alejandro Monge <alejandro@puntocommerce.com>
2022-03-25 12:36:09 -0600

    sending xml file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/xml/prices", (req, res) => {
+  fs.readFile("./precios.xml", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 app.listen(port, () => {
